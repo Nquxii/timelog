5147a2966b2e413e56b5d699c9f42e7c458cee07
Nquxii <ufq@pm.me>
2022-06-19 11:47:58 -0400

    minor adjustments

--- tlog.py
+++ tlog.py
@@ -12,10 +12,10 @@ logcsv = os.path.join(os.path.dirname(__file__), 'data.csv')
 # Create the parser
 my_parser = argparse.ArgumentParser(description='Log the user\'s current use of time in a csv file')
 
-# Add activity and description arguments
-my_parser.add_argument('-a', type=str, help='Current activity')
-my_parser.add_argument('-d', type=str, help='Description of action')
-my_parser.add_argument('-p', type=str, help='Classification of productivity')
+# Add activity and description arguments [all are optional]
+my_parser.add_argument('-a', metavar="ACTIVITY", type=str, help='add an activity')
+my_parser.add_argument('-d', metavar="DESC", type=str, help='description of the log')
+my_parser.add_argument('-i', metavar="IS_PRODUCTIVE", type=str, help='whether or not the log is considered productive')
 
 # Execute the parse_args() method
 args = my_parser.parse_args()
@@ -23,12 +23,13 @@ args = my_parser.parse_args()
 # Variables needed to add to the csv
 activity = args.a
 description = args.d
-productivity = args.p
+productivity = args.i
 
 # Time and date of the data
 time = dt.datetime.now().strftime('%I:%M %p')
 date = dt.datetime.today().strftime('%m-%d')
 
+# Set a variable for our existing csv time log
 existingData = pd.read_csv(logcsv)
 
 # What kind of analyses would I want to perform on my time mgmt data?
@@ -50,4 +51,4 @@ newLog = pd.DataFrame({
 # Append the new log of user data to the csv
 newLog.to_csv(logcsv, mode='a', header=False)
 
-print(f'{activity} at {time} added to data.csv.')
+print(f'{activity} at {time} added to {logcsv}')
